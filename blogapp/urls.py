de63a07d48eeced17ec8b67655b65b8e8284c029
Bonheur773 <bonheurngezayo4@gmail.com>
2023-10-14 14:45:50 +0300

    Deploying My Blog Website

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='blog')),
     #path('', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
